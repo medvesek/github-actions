0fafde2ff566bebb8ea0a9cb2df5e29375b1b147
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-12 21:24:01 +0200

    step summary

--- actions/netlify-deploy/src/index.js
+++ actions/netlify-deploy/src/index.js
@@ -1,11 +1,13 @@
 import * as core from "@actions/core";
 import createNetlifySite from "./action.js";
+import { appendFile } from "fs/promises";
 
 try {
   const authToken = core.getInput("token");
   const dir = core.getInput("dir");
   const customDomain = core.getInput("custom_domain");
-  await createNetlifySite({ authToken, dir, customDomain });
+  const site = await createNetlifySite({ authToken, dir, customDomain });
+  await appendFile(process.env.GITHUB_STEP_SUMMARY, site.url);
 } catch (error) {
   // Handle errors and indicate failure
   core.setFailed(error.message);

6f4be2affd02eb7230d9175044ab9905663b50d2
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-12 23:58:10 +0200

    default domain

--- actions/netlify-deploy/src/index.js
+++ actions/netlify-deploy/src/index.js
@@ -8,7 +8,11 @@ try {
   const customDomain = core.getInput("custom_domain");
   const site = await createNetlifySite({ authToken, dir, customDomain });
   core.setOutput("url", site.url);
+  core.setOutput("default_domain", site.default_domain);
   await appendFile(process.env.GITHUB_STEP_SUMMARY, site.url);
+  if (site.url !== site.default_domain) {
+    await appendFile(process.env.GITHUB_STEP_SUMMARY, site.default_domain);
+  }
 } catch (error) {
   // Handle errors and indicate failure
   core.setFailed(error.message);
